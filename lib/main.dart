6e55a5c60fda8a27f80a21f63560b21eb0888bca
Luca D'Aguanno <daguannoluca16gmail.com>
2021-08-27 12:22:18 +0200

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[Text('test')],
         ),
       ),
     );
